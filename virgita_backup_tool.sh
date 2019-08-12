5e91c652492db69f377aacbc2688787f76a6be6b
Christopher Naranjo <chrnaranjo@gmail.com>
2019-08-12 13:03:21 -0600

    Update Files

--- virgita_backup_tool.sh
+++ virgita_backup_tool.sh
@@ -6,6 +6,23 @@
 
 . virgita.conf
 
+
+#System Globals
+TarPath=$(which tar);if [ "$TarPath" = "" ];then echo "ERROR: I can not find tar command.";exit 1;fi
+HostName=$(hostname)
+DaTe=$(date +%d-%m-%Y)
+FullName="FULL-$HostName-$DaTe"
+DiffName="DIFF-$HostName-$DaTe"
+FullBackupTo="$BackupTo/$HostName"
+
+
+#Create backup Directory if not exists 
+if [ ! -d $FullBackupTo ];then
+	mkdir -p $FullBackupTo
+fi
+
+
+
 #System excludes
 SysEx="/proc,/dev,/sys,/tmp"
 Exclude=""
@@ -22,6 +39,15 @@ for i in ${UserExclude//,/ };do
 
 done
 
+#User exclude extensions
+for i in ${UserExcludeExt//,/ };do
+	
+	Exclude=$(printf  "%s "  "$Exclude --exclude="\'*.$i\'"")
+
+done
+
+
+
 #Backup Directories
 for i in ${BackupDirs//,/ };do
 	
@@ -34,12 +60,9 @@ Exclude="$Exclude --exclude=$BackupTo/*"
 
 
 
-FULL_COMMAND_GZ="tar -cvf /file.tgz $Exclude $Backup"
+echo "$TarPath -czvf $FullBackupTo/$FullName.tgz $Exclude  $Backup"
+$TarPath -czvf $FullBackupTo/$FullName.tgz $Exclude  $Backup
 
-
-
-
-echo "$FULL_COMMAND_GZ"
 
 
 
